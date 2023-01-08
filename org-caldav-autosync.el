bcb74d99128dd1a4c2e6c1b1391e2b6e491b6f8e
Jack Kamm <jackkamm@gmail.com>
2023-01-08 13:24:51 -0800

    Add dependency on org-caldav

--- org-caldav-autosync.el
+++ org-caldav-autosync.el
@@ -4,6 +4,7 @@
 
 ;; Author: Jack Kamm <jackkamm@gmail.com>
 ;; Keywords: calendar, caldav
+;; Package-Requires: (org-caldav)
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
